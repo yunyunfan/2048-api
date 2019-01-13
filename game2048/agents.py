946a6ccb6377001c615acbd046622b29afa60a2e
yunyunfan <44723981+yunyunfan@users.noreply.github.com>
2019-01-13 21:59:54 +0800

    Add files via upload

--- game2048/agents.py
+++ game2048/agents.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import torch
 
 class Agent:
     '''Agent Base.'''
@@ -46,3 +46,39 @@ class ExpectiMaxAgent(Agent):
     def step(self):
         direction = self.search_func(self.game.board)
         return direction
+
+class myAgent(Agent):
+
+    def __init__(self, game, display=None):
+        if game.size != 4:
+            raise ValueError(
+                "`%s` can only work with game of `size` 4." % self.__class__.__name__)
+        super().__init__(game, display)
+        device = torch.device('cuda')
+        model = torch.load('/mnt/H/yunyun/0112(552.96)/0112/mymodel2.pth')
+        model.to('cpu')
+        self.search_func = model
+        # self.game.board = torch.tensor(self.game.board, dtype=torch.float32)
+
+    def step(self):
+        board = self.preprocess()
+        direction = self.search_func(board)
+        maxid = np.where(direction == torch.max(direction))[1]
+        counts = np.bincount(maxid)
+        d = np.argmax(counts)
+
+        return int(d)
+
+    def preprocess(self):
+        board = self.game.board
+        board[board == 0] = 1
+        board = np.log2(board).flatten()
+        board = board.reshape(4, 4)
+        board = board[:, :np.newaxis]
+        board=board/11.0
+        #board = ToTensor()(board)
+        board = torch.from_numpy(board)
+        board = torch.unsqueeze(board, dim=0)
+        board = board.repeat(1, 1, 1, 1)
+        board = torch.tensor(board,dtype = torch.float32)
+        return board
